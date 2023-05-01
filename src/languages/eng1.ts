e9fb6e7ea294dd5ecb944fb08c45ad2ba2ae054a
Alexus <aleksprutik@gmail.com>
2023-05-02 00:00:06 +0200

    refactor: deleted commented code

--- src/languages/eng1.ts
+++ src/languages/eng1.ts
@@ -1,30 +1,3 @@
-/*const base = {
-  'Digit1': 'a',
-  'Digit2': 'b',
-  'Backspace': 'Backspace',
-  'ShiftLeft': 'Shift',
-  'CapsLock': 'Caps',
-  'Lang': 'Lang'
-}
-
-const shift = {
-  'Digit1': 'A',
-  'Digit2': 'B',
-  'Backspace': 'Backspace',
-  'ShiftLeft': 'Shift',
-  'CapsLock': 'Caps',
-  'Lang': 'Lang'
-}
-
-const caps = {
-  'Digit1': '+',
-  'Digit2': '-',
-  'Backspace': 'Backspace',
-  'ShiftLeft': 'Shift',
-  'CapsLock': 'Caps',
-  'Lang': 'Lang'
-}*/
-
 const base = {
   "Backquote": "`",
   "Digit1": "1",
