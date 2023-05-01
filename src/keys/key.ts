0dd2a0bbb2dc7c9da7b0a50f603368d0e34de3b9
Alexus <aleksprutik@gmail.com>
2023-05-01 19:35:15 +0200

    fix: mouse click in outside fixed

--- src/keys/key.ts
+++ src/keys/key.ts
@@ -11,12 +11,18 @@ export class Key extends Control {
     this.node.textContent = data;
     this.node.onmousedown = ()=>{
       this.down();
+      document.addEventListener('mouseup', ()=>{
+        this.input();
+        this.up();
+      }, {once: true});
     }
 
-    this.node.onmouseup = ()=>{
-      this.input();
-      this.up();
-    }
+    // this.node.onmouseup = ()=>{
+    //   this.input();
+    //   this.up();
+    // }
+
+
     
     this.node.onmouseenter = ()=>{
       this.node.classList.add('keyboard_key__hover');
@@ -44,7 +50,7 @@ export class Key extends Control {
   protected down(){
     this.node.classList.add('keyboard_key__down');
   }
-  
+
   protected up(){
     this.node.classList.remove('keyboard_key__down');
   }
